2f41fd96818df5a584d91b6126cf37576ac821d2
Kale Kundert <kale@thekunderts.net>
2014-12-20 23:42:51 -0800

    Consolidate muffler.py and cursor.py into io.py

--- io.py
+++ io.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+
 # This module provides some tools for manipulating the position of the cursor.  
 # This is useful for programs that need to update some simple pieces of 
 # information (like a clock) but don't require a full-blown ncurses interface.
+
+# This module defines a class which can be used to temporarily suppress stdout 
+# and stderr, using the natural 'with-statement' syntax.  Anything written to 
+# either file descriptor while it is being muffled can be accessed as a string.
 
 from __future__ import division
 from __future__ import print_function
@@ -23,6 +29,55 @@ styles = {
     'normal'        : 0,
     'bold'          : 1,
     'reverse'       : 2 }
+
+
+class Muffler(object):
+
+    class File:
+
+        def __init__(self):
+            self.string = ""
+
+        def __str__(self):
+            return self.string
+
+        def write(self, string):
+            self.string += string
+
+        def flush(self):
+            pass
+
+
+    def __init__(self, **files):
+        self.stdout = sys.stdout
+        self.stderr = sys.stderr
+
+        self.files = {
+                'stdout' : files['stdout'] if 'stdout' in files else True,
+                'stderr' : files['stderr'] if 'stderr' in files else True }
+
+        self.file = Muffler.File()
+
+    def __str__(self):
+        return str(self.file)
+
+    def __contains__(self, string):
+        return string in str(self)
+
+    def __enter__(self):
+        if self.files['stdout']:
+            sys.stdout = self.file
+            
+        if self.files['stderr']:
+            sys.stderr = self.file
+
+        return self
+
+    def __exit__(self, *ignore):
+        sys.stdout = self.stdout
+        sys.stderr = self.stderr
+
+
 
 def write(string):
     """ Write the given string to standard out. """
@@ -139,4 +194,15 @@ def terminal_size():
 
     return int(cr[1]), int(cr[0])
 
+
+if __name__ == '__main__':
+
+    muffler = Muffler()
+    greeting = "Hello world!"
+
+    with muffler:
+        print(greeting)
+
+    assert str(muffler) == greeting + '\n'
+    print("All tests passed!")
 
