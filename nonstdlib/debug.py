f7f1381c3c20eca5999dd7f7773cfc6ce199a21d
Kale Kundert <kale@thekunderts.net>
2015-08-04 21:41:47 -0700

    Improve the logging interface.
    Variable names from the calling scope are automatically substituted, and
    method names are no longer incorporated into logger names.

--- nonstdlib/debug.py
+++ nonstdlib/debug.py
@@ -4,66 +4,100 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-# This module provides a function that prints out the file, function, and line 
-# number that it was called from.  This is useful for debugging messages, which 
-# can sometimes be hard to find once the bug has been fixed.
+def log(level, message, **kwargs):
+    _log(level, message, **kwargs)
 
-import inspect, logging
-from pprint import pprint
+def debug(message, **kwargs):
+    _log(logging.DEBUG, message, **kwargs)
 
-logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s')
+def info(message, **kwargs):
+    _log(logging.INFO, message, **kwargs)
 
-def get_logger(frame_depth=1):
+def warning(message, **kwargs):
+    _log(logging.WARNING, message, **kwargs)
+
+def error(message, **kwargs):
+    _log(logging.ERROR, message, **kwargs)
+
+def critical(message, **kwargs):
+    _log(logging.CRITICAL, message, **kwargs)
+
+
+def _log(level, message, frame_depth=2, **kwargs):
+    """
+    Log the given message to the given level.  This function must be called 
+    from within another function, because it is hard-coded to use a frame depth 
+    of 2 to pick a name.
+    """
+    import inspect, logging
+
     try:
+        # Inspect variables two frames where we currently are (by default).  
+        # One frame up is assumed to be one of the helper methods defined in 
+        # this module, so we aren't interested in that.  Two frames up should 
+        # be the frame that's actually trying to log something.
+
         frame = inspect.stack()[frame_depth][0]
+        
+        # Collect all the variables in the scope of the calling code, so they 
+        # can be substituted into the message.
+
+        scope = {}
+        scope.update(frame.f_globals)
+        scope.update(frame.f_locals)
+
+        # If the calling frame is inside a class (deduced based on the presense 
+        # of a 'self' variable), name the logger after that class.  Otherwise 
+        # if the calling frame is inside a function, name the logger after that 
+        # function.
+
+        name = frame.f_globals['__name__']
+        self = frame.f_locals.get('self')
         function = inspect.getframeinfo(frame).function
-        module = frame.f_globals['__name__']
-        self = frame.f_locals.get('self', None)
-        location = str(self.__class__) if self else module
-        if function != '<module>': location += '.' + function
+
+        if self is not None:
+            name += '.' + self.__class__.__name__
+
+        elif function != '<module>':
+            name += '.' + function
 
     finally:
-        del frame
+        try: del frame
+        except UnboundLocalError: pass
 
-    return logging.getLogger(location)
-
-def set_log_level(lvl):
-    if isinstance(lvl, str): lvl = getattr(logging, str)
-    logging.getLogger().setLevel(lvl)
-
-def log(lvl, msg, *args, **kwargs):
-    get_logger(2).log(lvl, msg.format(*args, **kwargs))
-
-def debug(msg, *args, **kwargs):
-    get_logger(2).debug(msg.format(*args, **kwargs))
-
-def info(msg, *args, **kwargs):
-    get_logger(2).info(msg.format(*args, **kwargs))
-
-def warning(msg, *args, **kwargs):
-    get_logger(2).warning(msg.format(*args, **kwargs))
-
-def error(msg, *args, **kwargs):
-    get_logger(2).error(msg.format(*args, **kwargs))
-
-def exception(msg, *args):
-    get_logger(2).exception(msg.format(*args))
-
-def critical(msg, *args, **kwargs):
-    get_logger(2).critical(msg.format(*args, **kwargs))
+    logging.getLogger(name).log(level, message.format(**scope), **kwargs)
 
 
 if __name__ == '__main__':
-    warning("Module level")
+    import logging
 
-    def foo():
-        error("Function level")
+    logging.basicConfig(
+            format='%(levelname)s: %(name)s: %(message)s',
+            level=logging.DEBUG,
+    )
+
+    # Make sure there aren't any stupid typos in the public interface.
+
+    log(logging.INFO, "Variable level")
+    info("Info level")
+    debug("Debug level")
+    warning("Warning level")
+    error("Error level")
+    critical("Critical error")
+
+    # Make sure different scopes are properly incorporated into the output.
+
+    info("Module level")
+
+    def foo():  # (no fold)
+        info("Function level")
         pass
 
     foo()
 
-    class Bar:
+    class Bar:  # (no fold)
         def __init__(self):
-            critical("Method level")
+            info("Method level")
 
     Bar()
+
