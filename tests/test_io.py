53be5c9c86d544567f8171baba58128b5ad0502a
Kale Kundert <kale@thekunderts.net>
2015-11-06 14:32:12 -0800

    Make the tests compatible with python2.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import nonstdlib
 
 def test_capture_output():
