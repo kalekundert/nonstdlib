508a183529b079ab6f9475e3a2ca99e44c55f286
Kale Kundert <kale@thekunderts.net>
2015-11-07 06:04:54 -0800

    Bump to version 1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import subprocess, shlex
 # $ git tag <version>; git push
 # $ python setup.py sdist upload -r pypi
 
-version = '1.2'
+version = '1.3'
 distutils.core.setup(
         name='nonstdlib',
         version=version,
